3ab0996e3494c29c68c2da082d05a430c05806c2
ih3na <ih3na1911@gmail.com>
2023-02-18 19:21:07 +0530

    project intitialization

---
+++ frontend/src/components/hero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="banner">
+        <div class="background">
+            <img class=""/>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default{
+        components: {
+            Hero
+        }
+    }
+</script>
+
+<style>
+.background {
+    background-color: grey;
+}
+
+
+</style>
